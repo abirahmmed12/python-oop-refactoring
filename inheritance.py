9be7cabfa902ad2084e90b07c50e1dc96d9ee95c
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2026-02-03 13:34:46 +0600

    updating inheritance

--- inheritance.py
+++ inheritance.py
@@ -35,3 +35,74 @@ mngr= Manager("Rahat Ahmed", "M201", 90000, "IT Operations")
 
 dev1.displaay_info()
 mngr.displaay_info()
+
+
+
+
+class BasicPhne:
+    def __init__(self,brand):
+        self.brand=brand
+
+    def make_call(self,number):
+        print(f"{self.brand} theke {number} e call kora hocche")
+
+
+class SmartPhone(BasicPhne):
+    def __init__(self,brand,camera_mp):
+        super().__init__(brand)
+        self.camera_mp=camera_mp
+
+    def take_photo(self):
+        print(f'{self.brand} er {self.camera_mp} camera diye pic tola hocche')
+
+phone=BasicPhne('iphone 17 pro max')
+
+phone.make_call('343434')
+
+phone2=SmartPhone('iphone 17 pro max',50)
+phone2.take_photo()
+
+
+
+class Product():
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+
+    def display_details(self):
+        print(f"{self.name} price is {self.price}")
+
+
+class Electronics(Product):
+    def __init__(self, name, price,warranty_month):
+        super().__init__(name, price)
+        self.warranty_month=warranty_month
+
+    def check_warranty(self):
+        print(f"{self.name} has {self.warranty_month} month warranty")
+
+
+class Clothing(Product):
+    def __init__(self, name, price,size):
+        super().__init__(name, price)
+        self.size=size
+    def try_on(self):
+        print(f"{self.size}")
+
+
+laptop = Electronics("MacBook M2", 125000, 12)
+
+
+tshirt = Clothing("Polo T-Shirt", 1200, "XL")
+
+
+laptop.display_details() 
+laptop.check_warranty()  
+
+print("-" * 20)
+
+tshirt.display_details() 
+tshirt.try_on()          
+
+
+
